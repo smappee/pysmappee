aa4b21b52564e50e5cced19f27a1f4b6a87086e2
Bram Devooght <b.devooght@smappee.com>
2020-03-16 14:26:12 +0100

    add setup file

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bsmappee",
+    version="0.0.1",
+    author="Smappee",
+    author_email="support@smappee.com",
+    description="Offical Smappee dev API and MQTT python wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/smappee/pysmappee",
+    packages=setuptools.find_packages(),
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
