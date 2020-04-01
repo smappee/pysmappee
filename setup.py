1ce10a8ee4830874fca1a85e0192db71905bd16b
Bram Devooght <b.devooght@smappee.com>
2020-04-01 15:46:36 +0200

    setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="bsmappee",
+    name="pysmappee",
     version="0.0.1",
     author="Smappee",
     author_email="support@smappee.com",
