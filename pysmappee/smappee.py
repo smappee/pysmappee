43fc16ff89de2ad614046f417db270abb47e765d
Bram Devooght <b.devooght@smappee.com>
2020-05-20 14:01:34 +0200

    add locations without active device

--- pysmappee/smappee.py
+++ pysmappee/smappee.py
@@ -41,20 +41,19 @@ class Smappee(object):
     def load_service_locations(self, refresh=False):
         locations = self.smappee_api.get_service_locations()
         for service_location in locations['serviceLocations']:
-            if 'deviceSerialNumber' in service_location:
-                if service_location.get('serviceLocationId') in self._service_locations:
-                    # refresh the configuration
-                    sl = self.service_locations.get(service_location.get('serviceLocationId'))
-                    sl.load_configuration(refresh=refresh)
-                else:
-                    # Create service location object
-                    sl = SmappeeServiceLocation(service_location_id=service_location.get('serviceLocationId'),
-                                                device_serial_number=service_location.get('deviceSerialNumber'),
-                                                smappee_api=self.smappee_api,
-                                                farm=self._farm)
+            if service_location.get('serviceLocationId') in self._service_locations:
+                # refresh the configuration
+                sl = self.service_locations.get(service_location.get('serviceLocationId'))
+                sl.load_configuration(refresh=refresh)
+            else:
+                # Create service location object
+                sl = SmappeeServiceLocation(service_location_id=service_location.get('serviceLocationId'),
+                                            device_serial_number=service_location.get('deviceSerialNumber'),
+                                            smappee_api=self.smappee_api,
+                                            farm=self._farm)
 
-                    # Add sl object
-                    self.service_locations[service_location.get('serviceLocationId')] = sl
+                # Add sl object
+                self.service_locations[service_location.get('serviceLocationId')] = sl
 
     @property
     def username(self):
