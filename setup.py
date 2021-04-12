d0b3a6a2c3901a48e1a859d86a6a3ae7321b9174
Bram Devooght <b.devooght@smappee.com>
2021-04-12 10:13:27 +0200

    bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pysmappee",
-    version="0.2.23",
+    version="0.2.24",
     author="Smappee",
     author_email="support@smappee.com",
     description="Offical Smappee dev API and MQTT python wrapper",
